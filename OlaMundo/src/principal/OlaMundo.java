f8aa7dea5aa739b98c2c46d88ab9f978a199acec
GustavoSerafim10 <gustavovinicius56@gmail.com>
2023-08-02 17:33:49 -0300

    subindo arquivos da primeira aula de java

---
+++ OlaMundo/src/principal/OlaMundo.java
@@ -0,0 +1,15 @@
+package principal;
+
+// classe principal
+public class OlaMundo {
+	
+	//metodo principal, onde executa o programa
+	public static void main(String[] args) {
+		
+		//imprime uma linha e ao final salta uma nova linha
+		//print = imprimir; ln = New Line;
+		System.out.println("Hello world, Olá Mundo!");
+
+	}
+
+}
