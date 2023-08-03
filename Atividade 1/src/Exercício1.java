4b786c63b7565469dd1f5bf2373fedc13318fb17
GustavoSerafim10 <gustavovinicius56@gmail.com>
2023-08-03 14:42:51 -0300

    subindo arquivos das variáveis que realizei no Eclipse

---
+++ Atividade 1/src/Exercício1.java
@@ -0,0 +1,31 @@
+
+public class Exercício1 {
+
+	public static void main(String[] args) {
+		
+	int numero1;
+	numero1 = 50;
+	
+	int numero2;
+	numero2 = 100;
+	
+	String casa = "Me informaram que sua casa pegou fogo";
+	boolean falso = false;
+	
+	System.out.println(numero1);
+	System.out.println(numero2);
+	System.out.println(casa);
+	System.out.println(falso);
+	
+	System.out.println("****************");
+
+	String nome = " Gustavo ";
+	String Sobrenome = "Vinicius";
+	
+	System.out.print("Meu nome é: " + nome + Sobrenome);
+	
+	
+	
+	}
+
+}
