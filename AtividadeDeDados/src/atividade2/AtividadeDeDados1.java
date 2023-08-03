5ae2522fdd98d42b0bdc82b0e49363a0f6476a53
GustavoSerafim10 <gustavovinicius56@gmail.com>
2023-08-03 17:47:36 -0300

    realizei a atividade 2 da aula de hoje quinta feira

---
+++ AtividadeDeDados/src/atividade2/AtividadeDeDados1.java
@@ -0,0 +1,10 @@
+package atividade2;
+
+public class AtividadeDeDados1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ AtividadeDeDados/src/principal/AtividadeDeDados.java
@@ -0,0 +1,57 @@
+package principal;
+
+import java.util.Scanner;
+
+public class AtividadeDeDados {
+
+	public static void main(String[] args) {
+	
+		Scanner leia = new Scanner(System.in);
+		
+	//Vamos lá começar o exercício do professor
+		
+		String jogador;
+		int número;
+		double peso;
+		char sexo;
+		
+		System.out.println("Digite o nome do Jogador:");
+		jogador = leia.nextLine();
+		
+		System.out.println("Digite o número do jogador:");
+		número = leia.nextInt();
+
+		System.out.println("Digite o peso do Jogador:");
+		peso = leia.nextDouble();
+		
+		System.out.println("Digite o sexo do jogador (m/f):");
+		sexo = leia.next().charAt(0);
+		
+		//Agora será imprimidas na tela
+		
+		System.out.println("O nome do jogador digitado foi:" + jogador);
+		System.out.println("Qual o número do jogador:" + número);
+		System.out.println("Qual o peso do jogador:" + peso);
+		System.out.println("Qual o sexo do jogador:" + sexo);
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		leia.close();
+	}
+
+}
